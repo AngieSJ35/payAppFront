c5bb889eb0015df2931ebf1415622e254b35678e
angie <angie.sibaja@adelantatupago.com>
2025-07-21 02:16:21 -0500

    fix url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,5 +1,5 @@
 // src/api/config.ts
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://3.81.235.62:3000';
 
 export const defaultHeaders = {
     'Content-Type': 'application/json',
